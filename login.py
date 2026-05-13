01aae81eebc8a0ec6d0950f3f5022558c75b0569
indrek-png <indrek.noor@tktk.ee>
2026-05-13 13:31:13 +0300

    Implement user authentication in login.py

---
+++ login.py
@@ -0,0 +1,24 @@
+username = input("Sisesta kasutajanimi:")
+password = input("Sisesta parool:")
+
+""" if username == "user":
+    if password == "1234":
+        print("Welcome!")
+    else:
+        print("Access denied!")
+else:
+    print("Access denied!") """
+
+#and
+
+""" if username == "user" and password == "1234":
+    print("Welcome!")
+else:
+    print("Access denied!") """
+
+#or
+
+if username != "user" or password != "1234":
+    print("Welcome!")
+else:
+    print("Access denied!")
